cd047cb7c20fbe33c89ab87d41fa02311ff90bef
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-12 22:06:48 -0300

    apagando tudo

--- src/app/getBackend+api.ts
+++
@@ -1,5 +0,0 @@
-export async function GET(): Promise<Response> {
-  return Response.json({
-    message: "Resposta do backend mobile",
-  })
-}
--- src/app/returnUser+api.ts
+++
@@ -1,8 +0,0 @@
-export async function POST(request: Request): Promise<Response> {
-  const { email, password } = await request.json();
-  
-  return Response.json({
-    email,
-    password,
-  })
-}
